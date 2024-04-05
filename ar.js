8e46126af8d5dac88b990bbf9339d6a7e7cdf55d
Tikhon Savenkov <brumwellte@gmail.com>
2024-04-05 20:04:46 +0500

    add frontend

---
+++ ar.js
@@ -0,0 +1,34 @@
+function scrollToElement(elementId) {
+    var element = document.getElementById(elementId);
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+}
+function redirectToPage_1() {
+    window.location.href = 'Model_1.html';
+}
+function redirectToPage_2() {
+    window.location.href = 'Model_2.html';
+}
+function redirectToPage_3() {
+    window.location.href = 'Model_3.html';
+}
+function redirectToPage_4() {
+    window.location.href = 'Model_4.html';
+}
+function redirectToPage_5() {
+    window.location.href = 'Model_5.html';
+}
+function redirectToPage_6() {
+    window.location.href = 'Model_6.html';
+}
+function redirectToPage_7() {
+    window.location.href = 'Model_7.html';
+}
+function redirectToPage_8() {
+    window.location.href = 'Model_8.html';
+}
+function redirectToPage_9() {
+    window.location.href = 'Model_9.html';
+}
+function redirectToPage_10() {
+    window.location.href = 'Model_10.html';
+}
